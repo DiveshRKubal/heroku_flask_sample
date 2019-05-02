07c47511f1fa7cb4099229b05ba075e35f0e7e97
DiveshRKubal <36663489+DiveshRKubal@users.noreply.github.com>
2019-05-02 17:14:35 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,54 @@
 from flask import Flask
 from flask import request
-import json
+from nltk.corpus import sentiwordnet as swn
+import pandas as pd
+import text_normalizer as tn
+
 app = Flask(__name__)
+
+def sentiment_analysis(input_sentence, verbose=False):
+    # tokenize and POS tag text tokens
+    tagged_text = [(token.text, token.tag_) for token in tn.nlp(review)]
+    pos_score = neg_score = token_count = obj_score = 0
+    # get wordnet synsets based on POS tags
+    # get sentiment scores if synsets are found
+    for word, tag in tagged_text:
+        ss_set = None
+        if 'NN' in tag and list(swn.senti_synsets(word, 'n')):
+            ss_set = list(swn.senti_synsets(word, 'n'))[0]
+        elif 'VB' in tag and list(swn.senti_synsets(word, 'v')):
+            ss_set = list(swn.senti_synsets(word, 'v'))[0]
+        elif 'JJ' in tag and list(swn.senti_synsets(word, 'a')):
+            ss_set = list(swn.senti_synsets(word, 'a'))[0]
+        elif 'RB' in tag and list(swn.senti_synsets(word, 'r')):
+            ss_set = list(swn.senti_synsets(word, 'r'))[0]
+        # if senti-synset is found
+        if ss_set:
+            # add scores for all found synsets
+            pos_score += ss_set.pos_score()
+            neg_score += ss_set.neg_score()
+            obj_score += ss_set.obj_score()
+            token_count += 1
+
+    # aggregate final scores
+    final_score = pos_score - neg_score
+    norm_final_score = round(float(final_score) / token_count, 2)
+    final_sentiment = 'positive' if norm_final_score >= 0 else 'negative'
+    if verbose:
+        norm_obj_score = round(float(obj_score) / token_count, 2)
+        norm_pos_score = round(float(pos_score) / token_count, 2)
+        norm_neg_score = round(float(neg_score) / token_count, 2)
+        # to display results in a nice table
+        sentiment_frame = pd.DataFrame([[final_sentiment, norm_obj_score, norm_pos_score,
+                                         norm_neg_score, norm_final_score]],
+                                       columns=pd.MultiIndex(levels=[['SENTIMENT STATS:'],
+                                                                     ['Predicted Sentiment', 'Objectivity',
+                                                                      'Positive', 'Negative', 'Overall']],
+                                                             labels=[[0, 0, 0, 0, 0], [0, 1, 2, 3, 4]]))
+        print(sentiment_frame)
+
+    return final_sentiment
+
 
 @app.route('/sentiment_analysis')
 def hello_world():
