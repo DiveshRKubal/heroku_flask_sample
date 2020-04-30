c56047a282c72126e38eba908d1cf44bf15760fe
DiveshRKubal <36663489+DiveshRKubal@users.noreply.github.com>
2020-04-30 19:14:37 +0530

    Update new.py

--- new.py
+++ new.py
@@ -195,17 +195,17 @@ def sarcasm(sentence):
     return final_emotion
 
 
-import numpy as np
-import urllib
-import cv2
-def url_to_image(url):
-	# download the image, convert it to a NumPy array, and then read
-	# it into OpenCV format
-	resp = urllib.request.urlopen(url)
-	image = np.asarray(bytearray(resp.read()), dtype="uint8")
-	image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-	# return the image
-	return image
+# import numpy as np
+# import urllib
+# import cv2
+# def url_to_image(url):
+# 	# download the image, convert it to a NumPy array, and then read
+# 	# it into OpenCV format
+# 	resp = urllib.request.urlopen(url)
+# 	image = np.asarray(bytearray(resp.read()), dtype="uint8")
+# 	image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+# 	# return the image
+# 	return image
 
 def face(sentence):
 	
